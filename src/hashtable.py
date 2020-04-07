89e33fdf184624928a172c953a3215504bc54166
Jason Loomis <jason@jrloomis.com>
2020-04-07 12:46:05 -0400

    lecture solution

--- src/hashtable.py
+++ src/hashtable.py
@@ -7,6 +7,9 @@ class LinkedPair:
         self.value = value
         self.next = None
 
+    def __repr__(self):
+        return f"<{self.key}, {self.value}>"
+
 
 class HashTable:
     """
@@ -53,15 +56,31 @@ class HashTable:
 
         Fill this in.
         """
+        # index = self._hash_mod(key)
+
+        # if self.count == self.storage:
+        #     self.resize()
+        # if self.storage[index]:
+        #     print("exists")
+        # else:
+        #     self.storage[index] = LinkedPair(key, value)
+        #     self.count += 1
+
+        # ? from lecture
+        # Hashmod the key to find the bucket
         index = self._hash_mod(key)
 
-        if self.count == self.storage:
-            self.resize()
-        if self.storage[index]:
-            print("exists")
+        # Check if a pair already exists in the bucket
+        pair = self.storage[index]
+        if pair is not None:
+            # If so, overwrite the key/value and throw a warning
+            if pair.key != key:
+                print("Warning: Overwriting value")
+                pair.key = key
+            pair.value = value
         else:
+            # If not, Create a new LinkedPair and place it in the bucket
             self.storage[index] = LinkedPair(key, value)
-            self.count += 1
 
     def remove(self, key):
         """
@@ -71,12 +90,23 @@ class HashTable:
 
         Fill this in.
         """
+        # index = self._hash_mod(key)
+
+        # if self.storage[index]:
+        #     self.storage[index] == None
+        # else:
+        #     print("nope")
+
+        # ? from lecture
         index = self._hash_mod(key)
 
-        if self.storage[index]:
-            self.storage[index] == None
+        # Check if a pair exists in the bucket with matching keys
+        if self.storage[index] is not None and self.storage[index].key == key:
+            # If so, remove that pair
+            self.storage[index] = None
         else:
-            print("nope")
+            # Else print warning
+            print("Warning: Key does not exist")
 
     def retrieve(self, key):
         """
@@ -86,12 +116,24 @@ class HashTable:
 
         Fill this in.
         """
+        # index = self._hash_mod(key)
+
+        # if self.storage[index]:
+        #     if self.storage[index].key == key:
+        #         return self.storage[index].value
+        # else:
+        #     return None
+
+        # ? from lecture
+        # Get the index from hashmod
         index = self._hash_mod(key)
 
-        if self.storage[index]:
-            if self.storage[index].key == key:
-                return self.storage[index].value
+        # Check if a pair exists in the bucket with matching keys
+        if self.storage[index] is not None and self.storage[index].key == key:
+            # If so, return the value
+            return self.storage[index].value
         else:
+            # Else return None
             return None
 
     def resize(self):
@@ -113,26 +155,28 @@ if __name__ == "__main__":
     ht = HashTable(2)
 
     ht.insert("line_1", "Tiny hash table")
-    ht.insert("line_2", "Filled beyond capacity")
-    ht.insert("line_3", "Linked list saves the day!")
+    # ht.insert("line_2", "Filled beyond capacity")
+    # ht.insert("line_3", "Linked list saves the day!")
+
+    print(f"storage --> {ht.storage}")
 
     print("")
 
     # Test storing beyond capacity
     print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+    # print(ht.retrieve("line_2"))
+    # print(ht.retrieve("line_3"))
 
     # Test resizing
-    old_capacity = len(ht.storage)
-    ht.resize()
-    new_capacity = len(ht.storage)
+    # old_capacity = len(ht.storage)
+    # ht.resize()
+    # new_capacity = len(ht.storage)
 
-    print(f"\nResized from {old_capacity} to {new_capacity}.\n")
+    # print(f"\nResized from {old_capacity} to {new_capacity}.\n")
 
-    # Test if data intact after resizing
-    print(ht.retrieve("line_1"))
-    print(ht.retrieve("line_2"))
-    print(ht.retrieve("line_3"))
+    # # Test if data intact after resizing
+    # print(ht.retrieve("line_1"))
+    # print(ht.retrieve("line_2"))
+    # print(ht.retrieve("line_3"))
 
     print("")
